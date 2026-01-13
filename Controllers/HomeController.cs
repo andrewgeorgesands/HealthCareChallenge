1f1d8e4ac5d9b231fdc25a6d5dbc2ade358151d2
andrew.sands <andrew.sands@visma.com>
2026-01-13 13:46:33 +0100

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+using HealthCareChallenge.Models;
+
+namespace HealthCareChallenge.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult Login()
+    {
+        return Challenge(new AuthenticationProperties { RedirectUri = "/" }, OpenIdConnectDefaults.AuthenticationScheme);
+    }
+
+    public IActionResult Logout()
+    {
+        return SignOut(new AuthenticationProperties { RedirectUri = "/" }, CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using System.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddEnvironmentVariables();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+    })
+    .AddCookie()
+    .AddOpenIdConnect(options =>
+    {
+        options.Authority = builder.Configuration["VismaConnect:Authority"];
+        options.ClientId = builder.Configuration["VismaConnect:ClientId"];
+        options.ClientSecret = builder.Configuration["VismaConnect:ClientSecret"];
+
+        if (string.IsNullOrEmpty(options.ClientId) || string.IsNullOrEmpty(options.Authority) || string.IsNullOrEmpty(options.ClientSecret))
+        {
+             var debugView = builder.Configuration.GetDebugView();
+             throw new Exception($"Missing VismaConnect configuration. ClientId: '{options.ClientId}', Authority: '{options.Authority}', Secret: '{(string.IsNullOrEmpty(options.ClientSecret) ? "missing" : "present")}'. Configuration Debug View: {debugView}");
+        }
+
+        options.CallbackPath = "/signin-oidc";
+        options.SignedOutCallbackPath = "/signout-callback-oidc";
+        options.ResponseType = "code";
+        options.SaveTokens = true;
+        options.GetClaimsFromUserInfoEndpoint = true;
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            NameClaimType = "name",
+            RoleClaimType = "role"
+        };
+        options.Scope.Add("openid");
+        options.Scope.Add("profile");
+        options.Scope.Add("email");
+
+        if (builder.Environment.IsDevelopment())
+        {
+            var proxyUrl = builder.Configuration["VismaConnect:ProxyUrl"];
+            if (!string.IsNullOrEmpty(proxyUrl))
+            {
+                options.BackchannelHttpHandler = new HttpClientHandler
+                {
+                    Proxy = new WebProxy(proxyUrl),
+                    UseProxy = true,
+                    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+                };
+            }
+        }
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    // app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
